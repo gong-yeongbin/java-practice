bcdd103f4a7b1ae389bbd94ee4080a39d7469790
공영빈 <yb.gong@shareit.kr>
2026-05-07 08:52:40 +0900

    Add examples for HashMap, Hashtable, and Properties usage with database configuration

---
+++ src/ch15/sec04/exam02/HashtableExample.java
@@ -0,0 +1,42 @@
+package ch15.sec04.exam02;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashtableExample {
+  static void main(String[] args) {
+//    Map<String, Integer> map = new Hashtable<>();
+    Map<String, Integer> map = new HashMap<>();
+
+    Thread threadA = new Thread() {
+      @Override
+      public void run() {
+        for (int i = 1; i <= 1000; i++) {
+          map.put(String.valueOf(i), i);
+        }
+      }
+    };
+
+    Thread threadB = new Thread() {
+      @Override
+      public void run() {
+        for (int i = 1001; i <= 2000; i++) {
+          map.put(String.valueOf(i), i);
+        }
+      }
+    };
+
+    threadA.start();
+    threadB.start();
+
+    try {
+      threadA.join();
+      threadB.join();
+    } catch (Exception e) {
+    }
+
+    int size = map.size();
+    System.out.println("총 Entry 수: " + size);
+    System.out.println();
+  }
+}
