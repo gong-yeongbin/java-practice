ee7169c347ece49e742e9ca698cbf4dd725c63d3
공영빈 <yb.gong@shareit.kr>
2026-05-13 08:57:04 +0900

    Add functional interfaces and lambda examples for various use cases

---
+++ src/ch16/sec05/exam02/Person.java
@@ -0,0 +1,17 @@
+package ch16.sec05.exam02;
+
+public class Person {
+  public void ordering(Comparable comparable) {
+    String a = "홍길동";
+    String b = "김길동";
+
+    int result = comparable.compare(a, b);
+    if (result < 0) {
+      System.out.println(a + "이 " + b + "보다 앞에 있습니다.");
+    } else if (result > 0) {
+      System.out.println(b + "이 " + a + "보다 앞에 있습니다.");
+    } else {
+      System.out.println(a + "과 " + b + "은 같습니다.");
+    }
+  }
+}
